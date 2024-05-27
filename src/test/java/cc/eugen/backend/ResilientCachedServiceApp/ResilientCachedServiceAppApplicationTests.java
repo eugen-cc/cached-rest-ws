3bbff352ced131836b51125a5d4c0846c5c94c86
Eugen Gross <github@eugen.cc>
2024-05-28 00:44:05 +0200

    fix: Fixed basic Unit-Test

--- src/test/java/cc/eugen/backend/ResilientCachedServiceApp/ResilientCachedServiceAppApplicationTests.java
+++ src/test/java/cc/eugen/backend/ResilientCachedServiceApp/ResilientCachedServiceAppApplicationTests.java
@@ -1,9 +1,10 @@
 package cc.eugen.backend.ResilientCachedServiceApp;
 
+import cc.eugen.backend.rest.proxy.ResilientCachedProxyApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {ResilientCachedProxyApplication.class})
 class ResilientCachedServiceAppApplicationTests {
 
 	@Test
